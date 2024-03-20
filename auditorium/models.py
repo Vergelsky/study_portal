f91d901652f33ae9a1922cb60d392d16b85dbdf0
Vergelsky <graf.werger@gmail.com>
2024-03-20 13:01:35 +0700

    24.1.2 готово

---
+++ auditorium/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from users.models import NULLABLE
+
+class Course(models.Model):
+    name = models.CharField(max_length=120, verbose_name='Название')
+    description = models.CharField(max_length=600, verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', verbose_name='Превью', **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=120, verbose_name='Название')
+    description = models.CharField(max_length=600, verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', verbose_name='Превью', **NULLABLE)
+    video = models.FileField(upload_to='video/', verbose_name='Видео', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
+
+    def __str__(self):
+        return self.name
