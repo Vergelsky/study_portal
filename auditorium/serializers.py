2f946c7ca489546c2827ac0edc009e865b40a8ed
Vergelsky <graf.werger@gmail.com>
2024-03-26 16:42:08 +0700

    24.2.1 - готово, количество уроков в курсе отображается.

--- auditorium/serializers.py
+++ auditorium/serializers.py
@@ -4,9 +4,13 @@ from auditorium.models import Lesson, Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_qty = serializers.SerializerMethodField()
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lessons_qty(self, instance):
+        return instance.lesson_set.all().count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
