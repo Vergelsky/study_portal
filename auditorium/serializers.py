9828b08f850b6276fbb5269fd65ad81b956ab8e7
Vergelsky <graf.werger@gmail.com>
2024-03-26 18:36:10 +0700

    24.2.3 - готово, в курсах передаются уроки

--- auditorium/serializers.py
+++ auditorium/serializers.py
@@ -3,8 +3,15 @@ from rest_framework import serializers
 from auditorium.models import Lesson, Course
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_qty = serializers.SerializerMethodField()
+    lesson_set = LessonSerializer(many=True)
     class Meta:
         model = Course
         fields = '__all__'
@@ -12,8 +19,12 @@ class CourseSerializer(serializers.ModelSerializer):
     def get_lessons_qty(self, instance):
         return instance.lesson_set.all().count()
 
+    def create(self, validated_data):
+        lessons = validated_data.pop('lesson')
 
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
+        course_item = Course.objects.create(**validated_data)
+
+        for l in lessons:
+            Lesson.objects.create(**l, course=course_item)
+
+        return course_item
